b431255fb07d8bea28f2632ec602e49a4c5bc393
Selfay <chris.lee90@hotmail.co.uk>
2020-04-10 22:46:18 +0100

    adding array of characters

--- script.js
+++ script.js
@@ -34,7 +34,10 @@ for (let i = 0; i < 5; i++) {
 }
 
 
-const lowercase = 
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const specialChars = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
+  "~", "*", "?", ":","\""]
 
 
 if () {}
