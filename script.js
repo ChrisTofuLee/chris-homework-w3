c6fa91b30addb79b9335511a1187c82c13269ba1
Selfay <chris.lee90@hotmail.co.uk>
2020-04-12 23:57:46 +0100

    cleaning up code

--- script.js
+++ script.js
@@ -1,75 +1,3 @@
-/*/ Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-function generatePassword() {
-  return "TODO: Implement password generator";
-}
-
-// Write password to the #password input
-function writePassword() {
-  console.log("Ive been called");
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-//prompts
-const passwordLength = prompt("please enter length of password between 8 and 128")
-const lowerCaseSelector = confirm("Password Criteria to include: lower case")
-const upperCaseSelector = confirm("Password Criteria to include: upper case")
-const numberSelector = confirm("Password Criteria to include: numbers")
-const specialSelector = confirm("Password Criteria to include: special characters")
-
-//password length
-function writePassword() {
-  let passwordLength = 0
-
-    const passwordLength = prompt("please enter length of password between 8 and 128");
-    const passLenInt = parseInt(passwordLength);
-    
-    if (passwordLength > 8 && passwordLength < 128) {
-     
-    }else {
-      const passwordLength = prompt ("Outside of suggested length, please choose between 8 and 128 (last attempt)")
-      
-    }
-}
-
-const passwordLength = prompt("please enter length of password between 8 and 128");
-const passLenInt = parseInt(passwordLength);
-    
-    if (passwordLength > 8 && passwordLength < 128) {
-     
-    }else {
-      const passwordLength = prompt ("Outside of suggested length, please choose between 8 and 128 (last attempt)")
-      
-    }
-  }
-  const lowerCaseSelector = confirm("Password Criteria to include: lower case")
-  const upperCaseSelector = confirm("Password Criteria to include: upper case")
-  const numberSelector = confirm("Password Criteria to include: numbers")
-  const specialSelector = confirm("Password Criteria to include: special characters")
-
-
-
-const shuffled = highLettersList.sort(() => 0.5 - Math.random());
-
-  
-let password = []
-for (let ii = 0; ii < 6; ii++) {
-  password.push(shuffled[ii])
-}
-const final = password.join("")
-console.log(final)
-  */
-//-----------------------------------------------------------
-
-
 
 var generateBtn = document.querySelector("#generate");
 
@@ -98,10 +26,10 @@ function writePassword() {
       }
       //password criteria identifier
     if (passwordLength >= 8 && passwordLength <= 128) {
-    const lowerCaseSelector = confirm("Password Criteria to include: lower case")
-    const upperCaseSelector = confirm("Password Criteria to include: upper case")
-    const numberSelector = confirm("Password Criteria to include: numbers")
-    const specialSelector = confirm("Password Criteria to include: special characters")
+    const lowerCaseSelector = confirm("Would you like to include 'lowercase letters' in your password?")
+    const upperCaseSelector = confirm("Would you like to include 'uppercase letters' in your password?")
+    const numberSelector = confirm("Would you like to include 'numbers' in your password?")
+    const specialSelector = confirm("Would you like to include 'special characters' in your password?")
 
     let preDefined = [lowLettersList, highLettersList, numbersList, specialCharsList]
     let criteria = [lowerCaseSelector, upperCaseSelector, numberSelector, specialSelector];
@@ -125,16 +53,14 @@ function writePassword() {
       }
       
 // remove false statements from list
-      console.log(criteria)
     }
     for (let io = criteria.length-1; io--;) {
       if (criteria[io] === false) criteria.splice(io, 1);
       
     }
     let criteriaMerged = criteria[0].concat(criteria[1], criteria[2], criteria[3])
-    
-    console.log(criteriaMerged)
-    console.log(criteria)
+
+// multiplier to reach max character length
     const criteriaMString = criteriaMerged.toString()
     let criteriaStringMulti = criteriaMString.repeat(4)
     const criteriaFinal = criteriaStringMulti.split("")
@@ -145,107 +71,10 @@ function writePassword() {
   const shuffler = criteriaFinal.sort(() => 0.5 - Math.random());
  
       let shufflePassword = shuffler.slice(0, passwordLength);
-      console.log(shuffler)
-
-      console.log(shufflePassword)
       const finalPass = shufflePassword.join("")
-      console.log(finalPass)
-      alert(finalPass)
+
+      alert("Your password is " + finalPass)
 
       
     } 
 }
-function finalPassword () {}
-
-
-//---------------------------------------------------
-/*
-//password length
-function lengthIdentifier() {
-  const passwordLength = prompt("please enter length of password between 8 and 128")
-}
-if (passwordLength < 8 || passwordLength > 128) {
-  const passwordLengthRetry = prompt ("Outside of suggested length, please choose between 8 and 128")
-}
-
-
-
-//password criteria
-const criterias = [
-  lowerCaseSelector, upperCaseSelector, numberSelector, specialSelector
-];
-
-for (let i = 0; i < 5; i++) {
-  if (i == false) {
-    alert("no criteria selected")
-  }
-}
-
-
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-const specialChars = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
-  "~", "*", "?", ":","\""]
-
-
-if () {}
-
-
-
-let counter = 0;
-
-const counterSpan = document.getElementById("s1")
-
-const decrementButton = document.getElementById("decrement")
-const incrementButton = document.getElementById("increment")
-                                        // this is now the function label
-decrementButton.addEventListener("click", decrement);
-decrementButton.addEventListener("click", decrement);
-
-function decrement() {
-    counter--;
-    counterSpan.textContent = counter;
-}
-
-function increment() {
-    counter++;
-    counterSpan.textContent = counter;
-}
-
-
-
-
-
-while loop for password length fails
-function writePassword() {
-    const passwordLength = prompt("please enter length of password between 8 and 128");
-    const passLenInt = parseInt(passwordLength);
-    let passCondition = 1
-    while(passCondition != 0) 
-    if (passwordLength < 8 || passwordLength > 128){
-    const passwordLengthRetry = prompt ("Outside of suggested length, please choose between 8 and 128");
-    passCondition ++
-    
-    
-    } else{
-      passCondition = 0
-    }
-
-}
-
-
- for (let i = 0; i < 4; i++) {
-      if (criteria[i] == false) {
-        falseCount++
-      }
-     
-      if (falseCount == 4) {
-        alert("no criteria selected, please start again and select minimum one criteria")
-      }
-    }
-    console.log(criteria)
-    for (let ii = 0; ii < 4; ii++) {
-      if (criteria[ii] == true) {
-      criteria[ii] = preDefined[ii]
-    }
-*/
---
+++ test.js
@@ -0,0 +1,251 @@
+/*/ Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  return "TODO: Implement password generator";
+}
+
+// Write password to the #password input
+function writePassword() {
+  console.log("Ive been called");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+//prompts
+const passwordLength = prompt("please enter length of password between 8 and 128")
+const lowerCaseSelector = confirm("Password Criteria to include: lower case")
+const upperCaseSelector = confirm("Password Criteria to include: upper case")
+const numberSelector = confirm("Password Criteria to include: numbers")
+const specialSelector = confirm("Password Criteria to include: special characters")
+
+//password length
+function writePassword() {
+  let passwordLength = 0
+
+    const passwordLength = prompt("please enter length of password between 8 and 128");
+    const passLenInt = parseInt(passwordLength);
+    
+    if (passwordLength > 8 && passwordLength < 128) {
+     
+    }else {
+      const passwordLength = prompt ("Outside of suggested length, please choose between 8 and 128 (last attempt)")
+      
+    }
+}
+
+const passwordLength = prompt("please enter length of password between 8 and 128");
+const passLenInt = parseInt(passwordLength);
+    
+    if (passwordLength > 8 && passwordLength < 128) {
+     
+    }else {
+      const passwordLength = prompt ("Outside of suggested length, please choose between 8 and 128 (last attempt)")
+      
+    }
+  }
+  const lowerCaseSelector = confirm("Password Criteria to include: lower case")
+  const upperCaseSelector = confirm("Password Criteria to include: upper case")
+  const numberSelector = confirm("Password Criteria to include: numbers")
+  const specialSelector = confirm("Password Criteria to include: special characters")
+
+
+
+const shuffled = highLettersList.sort(() => 0.5 - Math.random());
+
+  
+let password = []
+for (let ii = 0; ii < 6; ii++) {
+  password.push(shuffled[ii])
+}
+const final = password.join("")
+console.log(final)
+  */
+//-----------------------------------------------------------
+
+
+
+var generateBtn = document.querySelector("#generate");
+
+const lowLetters = "abcdefghijklmnopqrstuvwxyz"
+const highLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowLettersList = lowLetters.split("")
+const highLettersList = highLetters.split("")
+const numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const specialCharsList = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
+  "~", "*", "?", ":","\""]
+
+generateBtn.addEventListener("click", writePassword);
+
+
+function writePassword() {
+
+  //password length identifier
+  const passwordLength = prompt("please enter length of password between 8 and 128");
+  const passLenInt = parseInt(passwordLength);
+    
+    if (passwordLength >= 8 && passwordLength <= 129) {
+     
+      }else {
+      alert("Outside of suggested length, please start again and enter a number between 8 and 128")
+     
+      }
+      //password criteria identifier
+    if (passwordLength >= 8 && passwordLength <= 128) {
+    const lowerCaseSelector = confirm("Password Criteria to include: lower case")
+    const upperCaseSelector = confirm("Password Criteria to include: upper case")
+    const numberSelector = confirm("Password Criteria to include: numbers")
+    const specialSelector = confirm("Password Criteria to include: special characters")
+
+    let preDefined = [lowLettersList, highLettersList, numbersList, specialCharsList]
+    let criteria = [lowerCaseSelector, upperCaseSelector, numberSelector, specialSelector];
+    let falseCount = 0
+
+// match selected criteria to predefined list
+    for (let i = 0; i < 4; i++) {
+      if (criteria[i] == false) {
+        falseCount++
+      }
+     
+      if (falseCount == 4) {
+        alert("no criteria selected, please start again and select minimum one criteria")
+      }
+    }
+    console.log(criteria)
+    for (let ii = 0; ii < 4; ii++) {
+      
+      if (criteria[ii] == true) {
+        criteria[ii] = preDefined[ii]
+      }
+      
+// remove false statements from list
+      console.log(criteria)
+    }
+    for (let io = criteria.length-1; io--;) {
+      if (criteria[io] === false) criteria.splice(io, 1);
+    }
+
+
+    console.log(criteria)
+//password shuffler + generator
+  const shuffler = criteria.sort(() => 0.5 - Math.random());
+  const criteriaString = criteria.toString()
+  console.log(criteriaString)
+  console.log(shuffler)
+  
+
+  /*let shufflePassword = []
+      for (let ia = 0; ia < 5; ia++) {
+        shufflePassword.push(shuffler[ia])
+      }*/
+      let shufflePassword = shuffler.slice(0, 6);
+      console.log(shuffler)
+
+      console.log(shufflePassword)
+      const finalPass = shufflePassword.join("")
+      console.log(finalPass)
+      alert(finalPass)
+
+      
+    } 
+}
+function finalPassword () {}
+
+
+//---------------------------------------------------
+/*
+//password length
+function lengthIdentifier() {
+  const passwordLength = prompt("please enter length of password between 8 and 128")
+}
+if (passwordLength < 8 || passwordLength > 128) {
+  const passwordLengthRetry = prompt ("Outside of suggested length, please choose between 8 and 128")
+}
+
+
+
+//password criteria
+const criterias = [
+  lowerCaseSelector, upperCaseSelector, numberSelector, specialSelector
+];
+
+for (let i = 0; i < 5; i++) {
+  if (i == false) {
+    alert("no criteria selected")
+  }
+}
+
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const specialChars = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
+  "~", "*", "?", ":","\""]
+
+
+if () {}
+
+
+
+let counter = 0;
+
+const counterSpan = document.getElementById("s1")
+
+const decrementButton = document.getElementById("decrement")
+const incrementButton = document.getElementById("increment")
+                                        // this is now the function label
+decrementButton.addEventListener("click", decrement);
+decrementButton.addEventListener("click", decrement);
+
+function decrement() {
+    counter--;
+    counterSpan.textContent = counter;
+}
+
+function increment() {
+    counter++;
+    counterSpan.textContent = counter;
+}
+
+
+
+
+
+while loop for password length fails
+function writePassword() {
+    const passwordLength = prompt("please enter length of password between 8 and 128");
+    const passLenInt = parseInt(passwordLength);
+    let passCondition = 1
+    while(passCondition != 0) 
+    if (passwordLength < 8 || passwordLength > 128){
+    const passwordLengthRetry = prompt ("Outside of suggested length, please choose between 8 and 128");
+    passCondition ++
+    
+    
+    } else{
+      passCondition = 0
+    }
+
+}
+
+
+ for (let i = 0; i < 4; i++) {
+      if (criteria[i] == false) {
+        falseCount++
+      }
+     
+      if (falseCount == 4) {
+        alert("no criteria selected, please start again and select minimum one criteria")
+      }
+    }
+    console.log(criteria)
+    for (let ii = 0; ii < 4; ii++) {
+      if (criteria[ii] == true) {
+      criteria[ii] = preDefined[ii]
+    }
+*/
