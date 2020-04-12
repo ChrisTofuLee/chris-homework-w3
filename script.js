008a9e872843a6f3568c75acde5bd2bedad38b72
Selfay <chris.lee90@hotmail.co.uk>
2020-04-12 20:02:56 +0100

    adding in password shuffler and criteria list compiler

--- script.js
+++ script.js
@@ -54,13 +54,32 @@ const passLenInt = parseInt(passwordLength);
   const upperCaseSelector = confirm("Password Criteria to include: upper case")
   const numberSelector = confirm("Password Criteria to include: numbers")
   const specialSelector = confirm("Password Criteria to include: special characters")
-*/
+
+
+
+const shuffled = highLettersList.sort(() => 0.5 - Math.random());
+
+  
+let password = []
+for (let ii = 0; ii < 6; ii++) {
+  password.push(shuffled[ii])
+}
+const final = password.join("")
+console.log(final)
+  */
 //-----------------------------------------------------------
 
 
 
 var generateBtn = document.querySelector("#generate");
-let criteria = [];
+
+const lowLetters = "abcdefghijklmnopqrstuvwxyz"
+const highLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowLettersList = lowLetters.split("")
+const highLettersList = highLetters.split("")
+const numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const specialCharsList = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
+  "~", "*", "?", ":","\""]
 
 generateBtn.addEventListener("click", writePassword);
 
@@ -71,10 +90,10 @@ function writePassword() {
     
     if (passwordLength >= 8 && passwordLength <= 129) {
      
-    }else {
+      }else {
       alert("Outside of suggested length, please start again and enter a number between 8 and 128")
             
-    }
+      }
     if (passwordLength >= 8 && passwordLength <= 128) {
     const lowerCaseSelector = confirm("Password Criteria to include: lower case")
     const upperCaseSelector = confirm("Password Criteria to include: upper case")
@@ -83,17 +102,42 @@ function writePassword() {
     
     let criteria = [lowerCaseSelector, upperCaseSelector, numberSelector, specialSelector];
     let falseCount = 0
+
     for (let i = 0; i < 4; i++) {
       if (criteria[i] == false) {
         falseCount++
       }
+
       if (falseCount == 4) {
         alert("no criteria selected, please start again and select minimum one criteria")
       }
     }
+    console.log(criteria)
+    let finalCriteriaList = [];
+    for (let i = 0; i < 4; i++) {
+      if (criteria[i] == true) {
+      finalCriteriaList.push(i)}
+      const lowerCaseSelector = lowLettersList
+      const upperCaseSelector = highLettersList
+      const numberSelector = numbersList
+      const specialSelector = specialCharsList
+
+      console.log(finalCriteriaList)
+      }
+
+      const shuffler = highLettersList.sort(() => 0.5 - Math.random());
+
+      let shufflePassword = []
+      for (let ii = 0; ii < 6; ii++) {
+        shufflePassword.push(shuffler[ii])
+      }
+      const finalPass = shufflePassword.join("")
+      console.log(finalPass)
+      }
 }
 }
 
+function finalPassword () {}
 
 
 //---------------------------------------------------
